5eb0da2aa31b64c71f0692b0e44e7bc4103d27f1
Woker-Heaven <superstar.geekboy@gmail.com>
2019-02-25 00:09:57 -0300

    Use basic tls handshake

--- client_ssl.py
+++ client_ssl.py
@@ -1,14 +1,22 @@
+#########################################################
+# The server and the client are using basic TLS handshake
+#########################################################
+
 import socket
 import ssl
 
 host_addr = '127.0.0.1'
 host_port = 8082
 server_sni_hostname = 'example.com'
-server_cert = 'server.crt'
 client_cert = 'client.crt'
 client_key = 'client.key'
 
-context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=server_cert)
+# NOTE: Get server's certificate in order to validate it
+server_cert = ssl.get_server_certificate((host_addr, host_port))
+print('server certificate: {}'.format(server_cert))
+
+# NOTE: Establish the SSL/TLS connection between the server and the client
+context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cadata=server_cert)
 context.load_cert_chain(certfile=client_cert, keyfile=client_key)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- server_ssl.py
+++ server_ssl.py
@@ -1,3 +1,7 @@
+#########################################################
+# The server and the client are using basic TLS handshake
+#########################################################
+
 import socket
 from socket import AF_INET, SOCK_STREAM, SO_REUSEADDR, SOL_SOCKET, SHUT_RDWR
 import ssl
@@ -6,12 +10,10 @@ listen_addr = '127.0.0.1'
 listen_port = 8082
 server_cert = 'server.crt'
 server_key = 'server.key'
-client_certs = 'client.crt'
 
+# NOTE: Configure the context with server's certificate and private key
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.verify_mode = ssl.CERT_REQUIRED
 context.load_cert_chain(certfile=server_cert, keyfile=server_key)
-context.load_verify_locations(cafile=client_certs)
 
 bindsocket = socket.socket()
 bindsocket.bind((listen_addr, listen_port))
